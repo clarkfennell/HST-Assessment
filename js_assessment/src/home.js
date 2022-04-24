b21104007b6b6dd3cdcb7599779b07832d1671de
Clark Fennell <clark.fennell@flauntdigital.com>
2022-04-24 17:08:01 +0100

    adding bootstrap and adjusting axios callback

--- js_assessment/src/home.js
+++ js_assessment/src/home.js
@@ -4,8 +4,8 @@ import CardList from './components/cardList'
 class Home extends Component {
     render() {
         return(
-            <div class="container">
-			    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4">
+            <div className="container">
+			    <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4">
                     <CardList />
                 </div>
             </div>
